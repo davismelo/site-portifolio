09a4e72c3a89267566b3fe540d870e14cae4c1ed
Davi Melo <davimeloz101@gmail.com>
2021-07-16 11:51:03 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,48 +1,24 @@
 const all_Items = document.querySelectorAll(".nav-item");
-const btn_mobile = document.querySelector("#checkbox-mobile");
+const btn_mobile = document.querySelector(".mobile-menu");
 const nav_bar = document.querySelector("nav");
-show_menu_btn = document.querySelector("#checkbox-mobile");
+const nav_text = document.querySelectorAll("header nav a");
 
 // MENU MOBILE
 
 const Close_Menu = () => {
-  nav_bar.classList.add("animation-hidden");
-  setTimeout(() => {
-    nav_bar.classList.remove("displayOn");
-    nav_bar.classList.add("displayNone");
-  }, 450);
+  nav_bar.classList.toggle("nav-active");
+  btn_mobile.classList.toggle("transform");
 };
 
-if (window.innerWidth < 620) {
-  all_Items.forEach((data) => {
-    data.addEventListener("click", () => {
-      Close_Menu();
-      btn_mobile.checked = false;
-    });
+all_Items.forEach((data) => {
+  data.addEventListener("click", () => {
+    Close_Menu();
   });
+});
 
-  show_menu_btn.addEventListener("click", () => {
-    if (show_menu_btn.checked) {
-      nav_bar.classList.remove("displayNone");
-      nav_bar.classList.remove("animation-hidden");
-      nav_bar.classList.add("displayOn");
-      nav_bar.classList.add("animation-show");
-    } else {
-      Close_Menu();
-    }
-  });
-}
-//---------------------------------------------------
-
-// PREENCHER A BARRA DE SKILLS
-
-const progress_bar = document.querySelectorAll(" .progress-data");
-
-progress_bar.forEach((element) => {
-  let p = element;
-  let value = element.textContent;
-  p.style.animation = "progress 2s";
-  p.style.width = value;
+btn_mobile.addEventListener("click", () => {
+  nav_bar.classList.toggle("nav-active");
+  btn_mobile.classList.toggle("transform");
 });
 //---------------------------------------------------
 
@@ -50,61 +26,93 @@ progress_bar.forEach((element) => {
 
 const arrow_left = document.querySelector("#arrow-left");
 const arrow_right = document.querySelector("#arrow-right");
-const slider_images = document.querySelector(".slider-images").children;
-const point_slide = document.querySelector("#slider-points").children;
-const images_array = [...slider_images];
-const point_array = [...point_slide];
+const slider_images = document.querySelectorAll(".slider-images li");
+const slider_dot = document.querySelectorAll(".slider-points div");
 
+slider_dot[0].classList.add("color-point-on");
 var index_slide = 0;
 
-for (let index = 0; index < images_array.length; index++) {
-  let value = images_array[index];
+for (let index = 0; index < slider_images.length; index++) {
+  let value = slider_images[index];
   if (index !== 0) {
     value.style.display = "none";
   }
 }
 
-for (let index = 0; index < point_array.length; index++) {
-  let element = point_array[index];
-  element.addEventListener("click", () => {
-    point_array.forEach((element) =>
-      element.classList.remove("color-point-on")
-    );
-    point_array[index].classList.add("color-point-on");
-
-    images_array.forEach((element) => {
+function Slide_Operations() {
+  function Hidden_Slides() {
+    slider_images.forEach((element) => {
       element.style.display = "none";
     });
-    images_array[index].style.display = "block";
+  }
+  function Show_Slides() {
+    slider_images[index_slide].style.display = "block";
+  }
+  Hidden_Slides();
+  Show_Slides();
+}
+
+function Add_Dot() {
+  slider_dot[index_slide].classList.add("color-point-on");
+}
+
+for (let index_slide = 0; index_slide < slider_dot.length; index_slide++) {
+  let element = slider_dot[index_slide];
+  element.addEventListener("click", () => {
+    slider_dot.forEach((element) => element.classList.remove("color-point-on"));
+    Add_Dot();
+
+    slider_images.forEach((element) => {
+      element.style.display = "none";
+    });
+    slider_images[index_slide].style.display = "block";
     return (index_slide = index);
   });
 }
 
 arrow_left.addEventListener("click", () => {
   if (index_slide > 0) {
-    images_array.forEach((element) => {
-      element.style.display = "none";
-    });
-    images_array[index_slide - 1].style.display = "block";
     index_slide--;
-
-    point_array[index_slide + 1].classList.remove("color-point-on");
-    point_array[index_slide].classList.add("color-point-on");
+    Slide_Operations();
+    slider_dot[index_slide + 1].classList.remove("color-point-on");
+    Add_Dot();
   }
 });
 
 arrow_right.addEventListener("click", () => {
-  if (index_slide < images_array.length - 1) {
+  if (index_slide < slider_images.length - 1) {
     index_slide++;
-    images_array.forEach((element) => {
-      element.style.display = "none";
-    });
-    images_array[index_slide].style.display = "block";
-
-    point_array[index_slide - 1].classList.remove("color-point-on");
-    point_array[index_slide].classList.add("color-point-on");
+    Slide_Operations();
+    slider_dot[index_slide - 1].classList.remove("color-point-on");
+    Add_Dot();
   }
 });
 //---------------------------------------------------
 
-// EFEITO
+// MUDAR COR DO HEADER
+const header = document.querySelector("header");
+const home = document.querySelector("#home");
+
+const Background_Header = () => {
+  if (window.scrollY >= home.offsetHeight - 20 && screen.width > 650) {
+    header.style.background = "#14191D";
+  } else {
+    header.style.background = "";
+  }
+};
+
+//-------------------------------------------------------
+
+// ANIMAÇÃO DE PREENCHIMENTO DA BARRA
+
+const progress_bar = document.querySelectorAll(" .progress-data");
+
+progress_bar.forEach((element) => {
+  let p = element;
+  let value = element.textContent;
+  p.style.width = value;
+});
+
+window.addEventListener("scroll", () => {
+  Background_Header();
+});
